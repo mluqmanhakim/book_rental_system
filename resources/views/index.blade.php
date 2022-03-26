1e9af1373ca0574720c2644c71055744571f9353
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2022-03-26 11:23:43 +0100

    Make migrations

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.base')
+
+@section('content')
+
+<div class="jumbotron">
+    <h1 class="display-4">Book Rental</h1>
+    <p class="lead">A place where you will laugh</p>
+</div>
+
+<div class="alert alert-primary" role="alert">
+Number of users
+</div>
+<div class="alert alert-primary" role="alert">
+Number of genres
+</div>
+<div class="alert alert-primary" role="alert">
+Number of books
+</div>
+<div class="alert alert-primary" role="alert">
+Number of active book rentals
+</div>
+<div class="alert alert-primary" role="alert">
+Number of active book rentals
+</div>
+
+
+<h1>List of genres</h1>
+
+<h1>Search books</h1>
+
+
+
+@endsection
