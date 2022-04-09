d4e886cd42f8b72d670ae5a8e6ea60e9b4b74645
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2022-04-09 18:41:13 +0200

    Edit view

--- resources/views/rental/detail.blade.php
+++ resources/views/rental/detail.blade.php
@@ -70,40 +70,49 @@
 </div>
 
 @if(Auth::user()->is_librarian())
-<h1>Set status and deadline</h1>
-<form action="{{ route('update_rental', $rental->id) }}" method="POST">
-    @csrf
-    @method('PUT')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">            
+            <h1>Set status and deadline</h1>
+            <form action="{{ route('update_rental', $rental->id) }}" method="POST">
+                @csrf
+                @method('PUT')
 
-    <div class="form-group">
-        <label for="status">Status</label>
-        <select name="status" class="form-control" id="status">
-            <option value="PENDING" {{ (old('status', $rental->status) == "PENDING" ? "selected":"") }}>PENDING</option>
-            <option value="REJECTED" {{ (old('status', $rental->status) == "REJECTED" ? "selected":"") }}>REJECTED
-            </option>
-            <option value="ACCEPTED" {{ (old('status', $rental->status) == "ACCEPTED" ? "selected":"") }}>ACCEPTED
-            </option>
-            <option value="RETURNED" {{ (old('status', $rental->status) == "RETURNED" ? "selected":"") }}>RETURNED
-            </option>
-        </select>
-    </div>
+                <div class="form-group mb-3 col-md-2">
+                    <label for="status">Status</label>
+                    <select name="status" class="form-control" id="status">
+                        <option value="PENDING" {{ (old('status', $rental->status) == "PENDING" ? "selected":"")
+                            }}>PENDING</option>
+                        <option value="REJECTED" {{ (old('status', $rental->status) == "REJECTED" ? "selected":"")
+                            }}>REJECTED
+                        </option>
+                        <option value="ACCEPTED" {{ (old('status', $rental->status) == "ACCEPTED" ? "selected":"")
+                            }}>ACCEPTED
+                        </option>
+                        <option value="RETURNED" {{ (old('status', $rental->status) == "RETURNED" ? "selected":"")
+                            }}>RETURNED
+                        </option>
+                    </select>
+                </div>
 
-    <div class="form-group">
-        <label for="deadline">Deadline</label>
-        <input name="deadline" type="datetime-local" class="form-control @error('deadline') is-invalid @enderror"
-            id="deadline" placeholder="" value="{{ old('deadline', $deadline) }}">
-        @error('deadline')
-        <div class="invalid-feedback">
-            {{ $message }}
+                <div class="form-group mb-3 col-md-2">
+                    <label for="deadline">Deadline</label>
+                    <input name="deadline" type="datetime-local"
+                        class="form-control @error('deadline') is-invalid @enderror" id="deadline" placeholder=""
+                        value="{{ old('deadline', $deadline) }}">
+                    @error('deadline')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                <div class="form-group mb-3">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </form>
         </div>
-        @enderror
     </div>
-
-    <div class="form-group">
-        <button type="submit" class="btn btn-primary">Save</button>
-    </div>
-
-</form>
+</div>
 @endif
-
 @endsection
